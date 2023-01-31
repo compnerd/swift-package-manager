54aca927965d5f1992a0dbf9784a90840fcbb7b0
Saleem Abdulrasool <compnerd@compnerd.org>
2023-01-31 11:14:18 -0800

    tests: `swift format` for edits

--- Tests/PackageModelTests/PackageModelTests.swift
+++ Tests/PackageModelTests/PackageModelTests.swift
@@ -11,64 +11,68 @@
 //===----------------------------------------------------------------------===//
 
 import Basics
-@testable import PackageModel
 import TSCBasic
 import TSCUtility
 import XCTest
 
+@testable import PackageModel
+
 class PackageModelTests: XCTestCase {
-    func testProductTypeCodable() throws {
-        struct Foo: Codable, Equatable {
-            var type: ProductType
-        }
-
-        func checkCodable(_ type: ProductType) {
-            do {
-                let foo = Foo(type: type)
-                let data = try JSONEncoder.makeWithDefaults().encode(foo)
-                let decodedFoo = try JSONDecoder.makeWithDefaults().decode(Foo.self, from: data)
-                XCTAssertEqual(foo, decodedFoo)
-            } catch {
-                XCTFail("\(error)")
-            }
-        }
-
-        checkCodable(.library(.automatic))
-        checkCodable(.library(.static))
-        checkCodable(.library(.dynamic))
-        checkCodable(.executable)
-        checkCodable(.test)
-    }
-    
-    func testProductFilterCodable() throws {
-        // Test ProductFilter.everything
-        try {
-            let data = try JSONEncoder().encode(ProductFilter.everything)
-            let decoded = try JSONDecoder().decode(ProductFilter.self, from: data)
-            XCTAssertEqual(decoded, ProductFilter.everything)
-        }()
-        // Test ProductFilter.specific(), including that the order is normalized
-        try {
-            let data = try JSONEncoder().encode(ProductFilter.specific(["Bar", "Foo"]))
-            let decoded = try JSONDecoder().decode(ProductFilter.self, from: data)
-            XCTAssertEqual(decoded, ProductFilter.specific(["Foo", "Bar"]))
-        }()
+  func testProductTypeCodable() throws {
+    struct Foo: Codable, Equatable {
+      var type: ProductType
     }
 
-    func testAndroidCompilerFlags() throws {
-        let triple = try Triple("x86_64-unknown-linux-android")
-        let sdkDir = AbsolutePath(path: "/some/path/to/an/SDK.sdk")
-        let toolchainPath = AbsolutePath(path: "/some/path/to/a/toolchain.xctoolchain")
-
-        let destination = Destination(
-            targetTriple: triple,
-            sdkRootDir: sdkDir,
-            toolchainBinDir: toolchainPath.appending(components: "usr", "bin")
-        )
-
-        XCTAssertEqual(try UserToolchain.deriveSwiftCFlags(triple: triple, destination: destination, environment: .process()), [
-            // Needed when cross‐compiling for Android. 2020‐03‐01
-            "-sdk", sdkDir.pathString,
-        ])
+    func checkCodable(_ type: ProductType) {
+      do {
+        let foo = Foo(type: type)
+        let data = try JSONEncoder.makeWithDefaults().encode(foo)
+        let decodedFoo = try JSONDecoder.makeWithDefaults().decode(Foo.self, from: data)
+        XCTAssertEqual(foo, decodedFoo)
+      } catch {
+        XCTFail("\(error)")
+      }
     }
+
+    checkCodable(.library(.automatic))
+    checkCodable(.library(.static))
+    checkCodable(.library(.dynamic))
+    checkCodable(.executable)
+    checkCodable(.test)
+  }
+
+  func testProductFilterCodable() throws {
+    // Test ProductFilter.everything
+    try {
+      let data = try JSONEncoder().encode(ProductFilter.everything)
+      let decoded = try JSONDecoder().decode(ProductFilter.self, from: data)
+      XCTAssertEqual(decoded, ProductFilter.everything)
+    }()
+    // Test ProductFilter.specific(), including that the order is normalized
+    try {
+      let data = try JSONEncoder().encode(ProductFilter.specific(["Bar", "Foo"]))
+      let decoded = try JSONDecoder().decode(ProductFilter.self, from: data)
+      XCTAssertEqual(decoded, ProductFilter.specific(["Foo", "Bar"]))
+    }()
+  }
+
+  func testAndroidCompilerFlags() throws {
+    let triple = try Triple("x86_64-unknown-linux-android")
+    let sdkDir = AbsolutePath(path: "/some/path/to/an/SDK.sdk")
+    let toolchainPath = AbsolutePath(path: "/some/path/to/a/toolchain.xctoolchain")
+
+    let destination = Destination(
+      targetTriple: triple,
+      sdkRootDir: sdkDir,
+      toolchainBinDir: toolchainPath.appending(components: "usr", "bin")
+    )
+
+    XCTAssertEqual(
+      try UserToolchain.deriveSwiftCFlags(
+        triple: triple, destination: destination, environment: .process()),
+      [
+        // Needed when cross‐compiling for Android. 2020‐03‐01
+        "-sdk", sdkDir.pathString,
+      ])
+  }
 }
